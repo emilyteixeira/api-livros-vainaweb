83cacdbcae803f5c00959a08ed4627daf41e673c
Emily Teixeira <milysillva@gmail.com>
2025-03-21 23:42:12 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify # Importando o Flask, request e jsonify
+import sqlite3 # Importando o sqlite3
+
+app = Flask(__name__) # Inicializando o Flask
+
+# Função para criar o banco de dados
+def init_db():
+  with sqlite3.connect('database.db') as conn:
+    conn.execute("""CREATE TABLE IF NOT EXISTS livros(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      titulo TEXT NOT NULL,
+      categoria TEXT NOT NULL,
+      autor TEXT NOT NULL,
+      imagem_url TEXT NOT NULL
+      )""")
+    print("Banco de dados criado!")
+
+# Chamando a função para criar o banco de dados
+init_db()
+
+# Rota para a página inicial
+@app.route('/')
+def home_page():
+  return '<h2>Livros Vai na Web</h2>'
+
+# Rota para cadastrar um novo livro
+@app.route('/doar', methods=['POST'])
+def doar():
+  
+  dados = request.get_json()
+  
+  titulo = dados.get('titulo')
+  categoria = dados.get('categoria')
+  autor = dados.get('autor')
+  imagem_url = dados.get('imagem_url')
+  
+  if not all([titulo, categoria, autor, imagem_url]):
+    return jsonify({'erro': 'Todos os campos são obrigatórios!'}), 400
+  
+  with sqlite3.connect('database.db') as conn:
+    conn.execute(""" INSERT INTO livros (titulo, categoria, autor, imagem_url)
+                 VALUES (?,?,?,?)
+                 """,(titulo, categoria, autor, imagem_url))
+    
+    conn.commit()
+    
+    return jsonify({"mensagem": "Livro cadastrado com sucesso!"}), 201
+  
+# Rota para listar todos os livros
+@app.route('/livros', methods=['GET'])
+def livros():
+  with sqlite3.connect('database.db') as conn:
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM livros""")
+    livros = cursor.fetchall()
+    
+    livros_json = []
+    
+    for livro in livros:
+      livro_id, titulo, categoria, autor, imagem_url = livro
+      livros_json.append({
+        'id': livro_id,
+        'titulo': titulo,
+        'categoria': categoria,
+        'autor': autor,
+        'imagem_url': imagem_url
+      })
+    
+    return jsonify(livros_json)
+
+# Tratamento de Erro
+if __name__ == '__main__':
+  app.run(debug=True)
+
